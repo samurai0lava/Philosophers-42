d760335cf08ee02a6e6df1b817f8b7e751dd9713
samurai0lava <ouhsseineilyass@gmail.com>
2024-09-16 12:50:53 +0100

    idk

--- main.c
+++ main.c
@@ -1,28 +1,57 @@
 #include "philo.h"
 
-static void init_struct(t_philo *philo)
+// static void init_struct(t_philo *philo)
+// {
+//     philo->id = 0;
+//     philo->left_fork = 0;
+//     philo->right_fork = 0;
+//     philo->eat_count = 0;
+//     philo->last_eat = 0;
+//     philo->is_eating = 0;
+//     philo->is_sleeping = 0;
+//     philo->is_thinking = 0;
+//     philo->is_dead = 0;
+//     philo->forks = NULL;
+//     philo->print = NULL;
+//     philo->eat = NULL;
+// }
+void *philo_routine()
 {
-    philo->id = 0;
-    philo->left_fork = 0;
-    philo->right_fork = 0;
-    philo->eat_count = 0;
-    philo->last_eat = 0;
-    philo->is_eating = 0;
-    philo->is_sleeping = 0;
-    philo->is_thinking = 0;
-    philo->is_dead = 0;
-    philo->forks = NULL;
-    philo->print = NULL;
-    philo->eat = NULL;
+    printf("lets_goooooooooooooooooo\n");
+    return (NULL);
+}
+void *philo_routine1()
+{
+    printf("wesh abatal\n");
+    return (NULL);
 }
 
-int main(int ac, char **av)
+int main()
 {
-    t_philo philo;
+    // t_philo philo;
+    pthread_t *thread1;
+    pthread_t *thread2;
 
-    if(ac < 5 || ac > 6)
+    thread2 = (pthread_t *)malloc(sizeof(pthread_t));
+    if (thread2 == NULL)
     {
-        init_struct(&philo);
+        printf("Error allocating memory\n");
+        return (1);
     }
+    thread1 = (pthread_t *)malloc(sizeof(pthread_t));
+    if (thread1 == NULL)
+    {
+        printf("Error allocating memory\n");
+        return (1);
+    }
+    if (pthread_create(thread1, NULL, &philo_routine, NULL) != 0)
+        return 1;
+    if (pthread_create(thread2, NULL, &philo_routine1, NULL) != 0)
+        return 1;
+
+    pthread_join(*thread1, NULL);
+    pthread_join(*thread2, NULL);
+    free(thread1);
+    free(thread2);
     return (0);
 }
