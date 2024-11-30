cd65a590e5cb36912766cb0b3645bed4fb29324b
Ilyass Ouhsseine <iouhssei@c3r2p4.1337.ma>
2024-11-30 15:08:02 +0100

    add

--- philov2/utils.c
+++ philov2/utils.c
@@ -6,7 +6,7 @@
 /*   By: iouhssei <iouhssei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 10:36:16 by iouhssei          #+#    #+#             */
-/*   Updated: 2024/11/30 14:38:52 by iouhssei         ###   ########.fr       */
+/*   Updated: 2024/11/30 15:07:37 by iouhssei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,11 @@ void wait_and_cleanup(t_philo *philos, pthread_t *threads)
         if (pthread_join(threads[i], NULL) != 0)
             printf("Error: Failed to join thread %d\n", i);
     }
+
     // Join monitor thread
     if (pthread_join(philos[0].shared_data.monitor_thread, NULL) != 0)
         printf("Error: Failed to join monitor thread\n");
+
     // Synchronize cleanup to avoid data races
     // pthread_mutex_lock(&philos[0].shared_data.cleanup_mutex);
     cleanup(philos);
@@ -57,11 +59,11 @@ void wait_and_cleanup(t_philo *philos, pthread_t *threads)
 
 void cleanup(t_philo *philos)
 {
-    // int i;
+    int i;
 
     if (philos == NULL)
         return;
-    // i = 0;
+    i = 0;
 	// while(i < philos[0].philo_data.numb_of_philos)
 	// {
 	// 	if (pthread_mutex_unlock(&philos[0].shared_data.forks[i]) != 0)
@@ -71,20 +73,19 @@ void cleanup(t_philo *philos)
 	// 	}
 	// 	i++;
 	// }
-	// i = 0;
+	i = 0;
 		// pthread_mutex_unlock(&philos[0].shared_data.forks[0]);
-    // while (i < philos[0].philo_data.numb_of_philos)
-    // {
-	// 	// pthread_mutex_unlock(&philos[0].shared_data.forks[i]);
-    //     if (pthread_mutex_destroy(&philos[0].shared_data.forks[i]) != 0)
-	// 	{
-	// 		// printf("%d\n", gettid());
-	// 		printf("failed to destroy : the forks mutex %d\n", i);
-	// 		return ;
-	// 	}
-    //     i++;
-    // }
-	
+    while (i < philos[0].philo_data.numb_of_philos)
+    {
+		// pthread_mutex_unlock(&philos[0].shared_data.forks[i]);
+        if (pthread_mutex_destroy(&philos[0].shared_data.forks[i]) != 0)
+		{
+			// printf("%d\n", gettid());
+			printf("failed to destroy : the forks mutex %d\n", i);
+			return ;
+		}
+        i++;
+    }
     if (pthread_mutex_destroy(philos[0].shared_data.print) != 0)
 	{
 		printf("failed to destroy : the print mutex\n");
