bddd5b549dd0afbce6aa18bfb3461134a2e86b6a
samurai0lava <ouhsseineilyass@gmail.com>
2024-12-28 14:07:53 +0100

    better monitor function

--- philo/monitor.c
+++ philo/monitor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   monitor.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iouhssei <iouhssei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: samurai0lava <samurai0lava@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 10:57:24 by iouhssei          #+#    #+#             */
-/*   Updated: 2024/12/28 11:12:42 by iouhssei         ###   ########.fr       */
+/*   Updated: 2024/12/28 14:07:23 by samurai0lav      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,32 @@ int	check_philo_death(t_philo *philo, long long current_time)
 	return (0);
 }
 
+static int check_philosopher_status(t_philo *philo)
+{
+    long long current_time;
+
+    current_time = get_time();
+    if (check_philo_death(philo, current_time) == 1)
+    {
+        printf_state(philo, PHILO_DEAD);
+        if (death_occured(philo) != 0)
+            return (-1);
+        return (1);
+    }
+    if (check_if_all_ate(philo))
+    {
+        if (death_occured(philo) != 0)
+            return (-1);
+        return (1);
+    }
+    return (0);
+}
+
 void *monitor(void *arg)
 {
-    t_philo      *philos;
-    int          i;
-    long long    current_time;
+    t_philo *philos;
+    int     i;
+    int     status;
 
     philos = (t_philo *)arg;
     while (1)
@@ -50,25 +71,17 @@ void *monitor(void *arg)
         i = 0;
         while (i < philos[0].philo_data.numb_of_philos)
         {
-            current_time = get_time();
-            if (check_philo_death(&philos[i], current_time) == 1)
-            {
-                printf_state(&philos[i], PHILO_DEAD);
-                if(death_occured(&philos[i]) != 0)
-					return(NULL);
+            status = check_philosopher_status(&philos[i]);
+            if (status != 0)
                 return (NULL);
-            }
-            if (check_if_all_ate(philos))
-            {
-                death_occured(&philos[i]);
-                return (NULL);
-            }
             i++;
         }
         usleep(1000);
     }
-    return (arg);
+    return (NULL);
 }
+
+// ... rest of the file remains unchanged ...
 
 
 int	check_is_dead(t_philo *philos)
@@ -80,6 +93,7 @@ int	check_is_dead(t_philo *philos)
 	status = (*philos->data.is_dead);
 	if (pthread_mutex_unlock(&philos->data.dead) != 0)
 		return (-1);
+
 	return (status);
 }
 
